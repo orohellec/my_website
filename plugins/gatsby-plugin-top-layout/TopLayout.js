59871e5b50bfb67976639b1f3f43c54455a65f8c
Olivier <orohellec44@gmail.com>
2019-12-20 22:09:52 +0100

    added gatsby-plugin-material-ui

--- plugins/gatsby-plugin-top-layout/TopLayout.js
+++ plugins/gatsby-plugin-top-layout/TopLayout.js
@@ -13,13 +13,8 @@ export default function TopLayout(props) {
           name="viewport"
           content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no"
         />
-        <link
-          href="https://fonts.googleapis.com/css?family=Roboto:400,500,700&display=swap"
-          rel="stylesheet"
-        />
       </Helmet>
       <ThemeProvider theme={theme}>
-        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
         <CssBaseline />
         {props.children}
       </ThemeProvider>
